bd88d85d797ef01d15c45f0754c1a9175f41a5fb
shevelev <andrey.shevelev@gorparkovki.ru>
2020-08-07 12:35:20 +0300

    test

---
+++ mylibrary/src/main/java/com/example/mylibrary/TestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.mylibrary
+
+import android.app.Activity
+
+class TestActivity: Activity() {
+
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/TestFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mylibrary
+
+import android.app.Activity
+import androidx.fragment.app.Fragment
+
+class TestFragment: Fragment() {
+
+}
